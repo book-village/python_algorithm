00eaef0ea7e2550b4037aefb517e0e8d2b4e8157
book-village <motomura.takaaki@gmail.com>
2022-10-17 19:33:54 +0900

    add linkedlist

---
+++ 03_単方向リンクリスト/linked_list.py
@@ -0,0 +1,60 @@
+class Node():
+    def __init__(self, data, next_node=None):
+        self.data = data
+        self.next = next_node
+
+
+class LinkedList():
+    def __init__(self, head=None):
+        self.head = head
+
+    def append(self, data):
+        new_node = Node(data)
+        if self.head is None:
+            self.head = new_node
+            return
+
+        last_node = self.head
+        while last_node.next:
+            last_node = last_node.next
+        last_node.next = new_node
+
+    def insert(self, data):
+        new_node = Node(data)
+        new_node.next = self.head
+        self.head = new_node
+
+    def print(self):
+        current_node = self.head
+        while current_node:
+            print(current_node.data)
+            current_node = current_node.next
+
+    def remove(self, data):
+        current_node = self.head
+        #  カレントノードが存在する、かつ
+        if current_node and current_node.data == data:
+            self.head = current_node.next
+            current_node = None
+            return
+
+        previous_node = None
+        while current_node and current_node.data != data:
+            previous_node = current_node
+            current_node = current_node.next
+
+        if current_node is None:
+            return
+
+        previous_node.next = current_node.next
+        current_node = None
+
+
+if __name__ == '__main__':
+    link = LinkedList()
+    link.append(1)
+    link.append(2)
+    link.append(3)
+    link.print()
+    link.remove(2)
+    link.print()
